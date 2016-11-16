4de0c44a60916d5d74ca626c58dd46cebb0ac945
libenchao <libenchao@gmail.com>
2016-11-17 00:39:12 +0800

    initialize code base and add NameNode, DataNode

---
+++ src/main/java/org/three_idiots/DataNode.java
@@ -0,0 +1,8 @@
+package org.three_idiots;
+
+public class DataNode {
+    public static void main(String args[]) {
+        System.out.println("starting DataNode ...");
+    }
+}
+
---
+++ src/main/java/org/three_idiots/NameNode.java
@@ -0,0 +1,8 @@
+package org.three_idiots;
+
+class NameNode {
+    public static void main(String args[]) {
+        System.out.println("starting NameNode ...");
+    }
+}
+
